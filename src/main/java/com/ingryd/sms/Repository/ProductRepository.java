831fec302cf4b3223d64bd2a01bb59e64b34521f
franklin efe <frankyx41@yahoo.com>
2023-10-02 22:18:11 +0100

    Created ProductRepository

--- src/main/java/com/ingryd/sms/Repository/ProductRepository.java
+++ src/main/java/com/ingryd/sms/Repository/ProductRepository.java
@@ -3,5 +3,10 @@ package com.ingryd.sms.Repository;
 import com.ingryd.sms.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findProductById(long id);
+    Optional<Product> findProductByCategory(String category);
+    Optional<Product> findProductByName(String name);
 }

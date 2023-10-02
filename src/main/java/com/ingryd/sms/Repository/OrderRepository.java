2a720887a44322fbc3e8137df0ff97222dfbcaf8
franklin efe <frankyx41@yahoo.com>
2023-10-02 23:07:48 +0100

    Created OrderRepository

--- src/main/java/com/ingryd/sms/Repository/OrderRepository.java
+++ src/main/java/com/ingryd/sms/Repository/OrderRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-
-    Optional<Product> findOrderById(long id);
+    Product findOrderById(long id);
 }

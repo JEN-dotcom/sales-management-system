2bce5bbac6c73ab0dabccdb634715797d2ec96ce
franklin efe <frankyx41@yahoo.com>
2023-10-02 23:02:15 +0100

    Created OrderRepository

--- src/main/java/com/ingryd/sms/Repository/OrderRepository.java
+++ src/main/java/com/ingryd/sms/Repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.ingryd.sms.Repository;
 
 import com.ingryd.sms.entity.Order;
+import com.ingryd.sms.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
+    Optional<Product> findOrderById(long id);
 }

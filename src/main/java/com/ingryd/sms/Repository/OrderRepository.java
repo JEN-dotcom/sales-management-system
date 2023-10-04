3149626fd067a75cc71e9d652d20119d32cb56ff
John Enyinwa <johnenyinwa@gmail.com>
2023-10-04 17:38:40 +0100

    added repository annotation to repo classes

--- src/main/java/com/ingryd/sms/Repository/OrderRepository.java
+++ src/main/java/com/ingryd/sms/Repository/OrderRepository.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByDateBetween(Date startOfDay, Date endOfDay, PageRequest pageRequest);
--- src/main/java/com/ingryd/sms/repository/OrderItemRepository.java
+++ src/main/java/com/ingryd/sms/repository/OrderItemRepository.java
@@ -1,9 +1,11 @@
 package com.ingryd.sms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ingryd.sms.entity.OrderItem;
 
+@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
